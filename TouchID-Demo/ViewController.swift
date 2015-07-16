4fa0c23e826e7aaf4f3db5bef0c07d0cb936d9b7
Roman Voglhuber <romanvoglhuber@mbp-vog.home>
2015-07-16 12:05:06 +0200

    Initial Commit

---
+++ TouchID-Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TouchID-Demo
+//
+//  Created by Roman Voglhuber on 16/07/15.
+//  Copyright (c) 2015 Voglhuber. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
